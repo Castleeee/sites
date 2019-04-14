c03e2c585b9c6af493aa8355d913086207dfdc8a
“owl <576359186@qq.com>
2019-04-14 20:25:01 +0800

    add data-04

---
+++ docs/.vuepress/components/books.vue
@@ -0,0 +1,21 @@
+<template>
+  <a :href='getbook()'>{{ bookname }}</a>
+</template>
+
+<script>
+  export default {
+    name: 'books',
+    props: ['bookpath', 'bookname'],
+    methods: {
+      getbook: function () {
+        var IpAddr = document.location.hostname
+        console.log(IpAddr + this.bookpath)
+        return 'http://'+ IpAddr+ "8888" + this.bookpath
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
