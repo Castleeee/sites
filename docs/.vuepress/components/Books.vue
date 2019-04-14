086cd232968db3c12be137c72a02bbdc991d5266
“owl <576359186@qq.com>
2019-04-14 20:52:56 +0800

    add file-05

--- docs/.vuepress/components/Books.vue
+++ docs/.vuepress/components/Books.vue
@@ -1,16 +1,17 @@
 <template>
-  <a :href='getbook()'>{{ bookname }}</a>
+  <div id="books"><a :href='getbook()'>{{ bookname }}</a></div>
+
 </template>
 
 <script>
   export default {
-    name: 'books',
+    name: 'Books',
     props: ['bookpath', 'bookname'],
     methods: {
       getbook: function () {
         var IpAddr = document.location.hostname
         console.log(IpAddr + this.bookpath)
-        return 'http://'+ IpAddr+ "8888" + this.bookpath
+        return 'http://'+ IpAddr+ ":8888" + this.bookpath
       }
     }
   }
